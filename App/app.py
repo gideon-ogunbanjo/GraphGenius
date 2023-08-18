c2ef36bf19aeac3e77efa93b24e85ce3eba1277f
Gideon-Ogunbanjo <dejiogunbanjo9@gmail.com>
2023-08-18 10:49:00 +0100

    modified application UI

--- App/app.py
+++ App/app.py
@@ -1,7 +1,16 @@
 import streamlit as st
 
+# Streamlit UI
+st.set_page_config(
+    page_title="GraphGenius",
+    initial_sidebar_state="expanded",
+    layout="centered"
+)
+
+
 def main():
-    st.title("Data Visualization Dashboard")
+    st.title("GraphGenius - Data Visualization Dashboard")
+    st.write("GraphGenius is a data visualization dashboard. Users can upload CSV or Excel files to visualize and explore their data easily!")
 
 if __name__ == "__main__":
     main()
