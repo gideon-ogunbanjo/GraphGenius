6ff2df273b0d88a76e68aed491c2c7d0b39db913
Gideon-Ogunbanjo <dejiogunbanjo9@gmail.com>
2023-08-18 16:11:38 +0100

    made minor changes

--- App/app.py
+++ App/app.py
@@ -6,11 +6,10 @@ import io
 
 # Streamlit UI
 st.set_page_config(
-    page_title="GraphGenius - Data Visualization & Exploration Dashboard",
-    initial_sidebar_state="expanded",
-    layout="centered"
+    page_title="GraphGenius",
+    layout="centered",
+    initial_sidebar_state="collapsed"
 )
-
 st.title("GraphGenius - Data Visualization & Exploration Dashboard")
 st.write("GraphGenius is a data visualization & Exploration dashboard. Users can upload CSV or Excel files to visualize, explore, and manipulate their data easily!")
 
