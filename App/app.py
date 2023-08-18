f3e7918d0e62890761e1e2a3e5d7e4bbe641f032
Gideon-Ogunbanjo <dejiogunbanjo9@gmail.com>
2023-08-18 10:54:30 +0100

    added File Uploader widget

--- App/app.py
+++ App/app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 # Streamlit UI
 st.set_page_config(
@@ -11,6 +12,20 @@ st.set_page_config(
 def main():
     st.title("GraphGenius - Data Visualization Dashboard")
     st.write("GraphGenius is a data visualization dashboard. Users can upload CSV or Excel files to visualize and explore their data easily!")
+    
+    # File Uploader Widget
+    uploaded_file = st.file_uploader("Upload a CSV or Excel file", type=["csv", "xlsx"])
+    
+    if uploaded_file is not None:
+        if uploaded_file.name.endswith('csv'):
+            data = pd.read_csv(uploaded_file)
+        elif uploaded_file.name.endswith('xlsx'):
+            data = pd.read_excel(uploaded_file, engine='openpyxl')
+        else:
+            st.error("Unsupported file format. Please upload a CSV or Excel file.")
+            return
+        
+        st.write("Uploaded Data:", data)
 
 if __name__ == "__main__":
     main()
